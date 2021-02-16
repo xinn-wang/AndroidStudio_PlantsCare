44c27ecac7bd125414c9ef1b93055b8ef952cb12
Kinh Truong <kinh.truong@metropolia.fi>
2021-02-16 11:22:01 +0200

    Added navigation function for main activity(homepage) Comment out logo string(DON'T PANIC..) in strings.xml file due to error

--- app/src/main/java/fi/group11/plantscare/MainActivity.java
+++ app/src/main/java/fi/group11/plantscare/MainActivity.java
@@ -1,8 +1,11 @@
-package fi.xinw.plantscare_group11;
+package fi.group11.plantscare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    // Homepage button methods to launch corresponding activity
+    public void myplantBtn(View v) {
+        Intent myplant = new Intent(this, MyPlantActivity.class);
+        startActivity(myplant);
+    }
+
+    public void reminderBtn(View v) {
+        Intent reminder = new Intent(this, ReminderActivity.class);
+        startActivity(reminder);
+    }
+
+    public void historyBtn(View v) {
+        Intent history = new Intent(this, HistoryActivity.class);
+        startActivity(history);
     }
 }
