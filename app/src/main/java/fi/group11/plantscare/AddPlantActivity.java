52aece13afd58b6606d8edd719509a5529ed9193
Kinh Truong <kinh.truong@metropolia.fi>
2021-02-20 15:34:36 +0200

    Created required activity and added navigation(set button onClick methods to launch intent) between all activities except for plant specification activity

--- app/src/main/java/fi/group11/plantscare/AddPlantActivity.java
+++ app/src/main/java/fi/group11/plantscare/AddPlantActivity.java
@@ -7,22 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-
-public class HistoryActivity extends AppCompatActivity {
+public class AddPlantActivity extends AppCompatActivity {
     private ImageButton backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history);
-        // back to home page onClick
+        setContentView(R.layout.activity_addplants);
+
         backBtn = findViewById(R.id.backBtn);
+        //lauch/back to my plant activity
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent back = new Intent(HistoryActivity.this, MainActivity.class);
-                startActivity(back);
+                Intent myplant = new Intent(AddPlantActivity.this, MyPlantActivity.class);
+                startActivity(myplant);
             }
         });
-
     }
 }
